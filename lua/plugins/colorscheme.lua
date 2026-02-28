907af6f2cfdc1bf29e93fd1a2e93ee773157a7d5
Tharun K <tharun9832@gmail.com>
2026-02-28 21:35:11 +0530

    Update plugins and change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,9 @@
 return {
-  "Shatur/neovim-ayu",
+  'sainnhe/gruvbox-material',
   lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme("ayu-mirage")
-  end,
+    vim.g.gruvbox_material_transparent_background = true
+    vim.cmd("colorscheme gruvbox-material")
+  end
 }
