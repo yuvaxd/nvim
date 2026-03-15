aab792c108f7df689e49c893ee8ed6eb04737a48
Tharun K <tharun9832@gmail.com>
2026-03-15 18:42:25 +0530

    Update plugins and change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,11 @@
 return {
-  "rose-pine/neovim",
-  name = "rose-pine",
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
   config = function()
-    vim.cmd("colorscheme rose-pine")
+    require("catppuccin").setup({
+      transparent_background = true
+    })
+    vim.cmd.colorscheme "catppuccin-nvim"
   end
 }
