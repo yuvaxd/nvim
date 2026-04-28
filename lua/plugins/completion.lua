dc02e40f33034c6f3999770381d424a638780990
Tharun K <tharun9832@gmail.com>
2026-04-28 12:01:26 +0530

    Enable auto show documentation in blink config

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -14,7 +14,7 @@ return {
     },
 
     completion = {
-      documentation = { auto_show = false },
+      documentation = { auto_show = true, auto_show_delay_ms = 500 },
       menu = {
         draw = {
           columns = {
